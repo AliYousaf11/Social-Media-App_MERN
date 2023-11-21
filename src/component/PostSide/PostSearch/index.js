5bccdac67251c5d7c5e69dab93e871db554d686e
Muhammad Ali Yousaf <100188537+AliYousaf11@users.noreply.github.com>
2023-11-21 18:03:21 +0500

    done upload photo

--- src/component/PostSide/PostSearch/index.js
+++ src/component/PostSide/PostSearch/index.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./PostSearch.css";
 import ProfileImg from "../../../img/profileImg.jpg";
 import UilScenery from "@iconscout/react-unicons/icons/uil-scenery";
 import UilPlayCircle from "@iconscout/react-unicons/icons/uil-play-circle";
 import UilLocationPoint from "@iconscout/react-unicons/icons/uil-location-point";
 import UilSchedule from "@iconscout/react-unicons/icons/uil-schedule";
+import UilTimes from "@iconscout/react-unicons/icons/uil-times";
 
-const index = () => {
+const Index = () => {
+  const [image, setImage] = useState(null);
+  const imageRef = useRef();
+
+  const HandlePhotoChange = (event) => {
+    if (event.target.files && event.target.files[0]) {
+      let img = event.target.files[0];
+      setImage({
+        image: URL.createObjectURL(img),
+      });
+    }
+  };
   return (
     <div className="PostSearch">
       <img src={ProfileImg} alt="Profile_img" />
@@ -15,7 +27,11 @@ const index = () => {
         <input type="text" placeholder="What's happenings?" />
 
         <div className="postOption">
-          <div className="option" style={{ color: "var(--photo)" }}>
+          <div
+            className="option"
+            style={{ color: "var(--photo)" }}
+            onClick={() => imageRef.current.click()}
+          >
             <UilScenery /> Photo
           </div>
           <div className="option" style={{ color: "var(--video)" }}>
@@ -28,10 +44,26 @@ const index = () => {
             <UilSchedule /> Shedule
           </div>
           <button className="button ps-button">Share</button>
+          <div style={{ display: "none" }}>
+            <input
+              type="file"
+              name="myImage"
+              ref={imageRef}
+              onChange={HandlePhotoChange}
+            />
+          </div>
         </div>
+
+        {image && (
+          <div className="previewImage">
+            <UilTimes onClick={() => setImage(null)} />
+            <img src={image.image} alt="previewImage" />
+          </div>
+        )}
+        
       </div>
     </div>
   );
 };
 
-export default index;
+export default Index;
