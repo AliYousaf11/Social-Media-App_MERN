3fb3310c94d4e974c1f20cb0ebb261686b075c15
SoftwareDevelopersPro <75448023+SoftwareDevelopersPro@users.noreply.github.com>
2023-12-19 01:35:13 +0500

    added live link for backend

--- src/api/AuthRequest.js
+++ src/api/AuthRequest.js
@@ -1,5 +1,6 @@
 import axios from "axios"
-const Api = axios.create({baseURL:"http://localhost:8080"})
+// const Api = axios.create({baseURL:"http://localhost:8080"})
+const Api = axios.create({ baseURL: "https://social-media-app-mern-backend-seven.vercel.app/" })
 
-export const login = (formData) => Api.post("/auth/login",formData)
-export const Signup = (formData) => Api.post("/auth/register",formData)
+export const login = (formData) => Api.post("/auth/login", formData)
+export const Signup = (formData) => Api.post("/auth/register", formData)
